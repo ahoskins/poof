5b4d5b028e689489090e081011a0e4c43c17fa79
Andrew Hoskins <andrew.hoskins16@gmail.com>
2015-01-26 21:09:43 -0700

    folder structure, license

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
-from setuptools import setup
+from distutils.core import setup
 
 setup(
 	name='poof',
-	version='0.1',
+	author='Andrew Hoskins',
+	auther_email='arh.hoskins@gmail.com'
+	version = '0.1',
+	keywords= ['workspace', 'command-line']
+	description='A command line workspace manager for Mac OS X',
 	py_modules=['poof', 'storage', 'space'],
 	install_requires=[
 		'Click',
