2229f33d0d6b5d228fdbd9cec0914c2384ca88b8
Andrew Hoskins <andrew.hoskins16@gmail.com>
2015-01-27 22:57:20 -0700

    fix the bad version convention

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='poof',
-    version='0.2',
+    version='0.1.0',
     packages=['poof'],
     install_requires=[
         'Click', 'fuzzywuzzy', 'python-Levenshtein'
